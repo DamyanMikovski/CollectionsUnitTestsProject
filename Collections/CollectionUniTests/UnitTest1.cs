44bd1ffabcf1ca9184fe306a4132eba6b9dee053
Damyan Mikovski <damyan.mikovski@concordeu.com>
2023-01-24 19:03:27 +0200

    Create Maser Branch for Project Collections

---
+++ Collections/CollectionUniTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CollectionUniTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
